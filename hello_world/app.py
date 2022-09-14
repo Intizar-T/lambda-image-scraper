5719bc1b3f1a151fd7d4a40141b87dcefd59c805
Intizar-T <intizar.tashov22@gmail.com>
2022-09-14 16:22:16 +0900

    initial commit

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,35 +1,24 @@
-import json
-import sys
-import os
 import io
 import time
 import requests
 import random
-import base64
-import urllib3
-import boto3
 
-from botocore.exceptions import ClientError
 from zipfile import ZipFile
-from datetime import datetime
 from tempfile import mkdtemp
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException, NoSuchWindowException
 
 language_list = ['en']
 
 class ImageScraper:
 
     def __init__(self):
-        self.driver = webdriver.Chrome("/opt/chromedriver", options=self.__get_default_chrome_options())
+        self.driver = webdriver.Chrome("../chromedriver", options=self.__get_default_chrome_options())
         
     def get_image_urls(self, query: str, max_urls: int, sleep_between_interactions: int = 1):
-        search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
+        search_url = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query={q}"
+        # "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
         self.driver.get(search_url.format(q=query))
 
         image_urls = set()
@@ -37,19 +26,19 @@ class ImageScraper:
         results_start = 0
         while image_count < max_urls:
             self.__scroll_to_end(sleep_between_interactions)
-            thumbnail_results = self.driver.find_elements(By.CSS_SELECTOR, "img.Q4LuWd")
+            thumbnail_results = self.driver.find_elements(By.CSS_SELECTOR, "img._image._listImage")
             number_results = len(thumbnail_results)
             print("Found: {0} search results. Extracting links from {1}:{0}".format(number_results, results_start))
 
             for img in thumbnail_results[results_start:number_results]:
                 self.__click_and_wait(img, sleep_between_interactions)
-                self.__add_image_urls_to_set(image_urls)
+                self.__add_image_urls_to_set(image_urls, max_urls)
                 image_count = len(image_urls)
                 if image_count >= max_urls:
-                    # print("Found: {} image links, done!".format(len(image_urls)))
+                    print("Found: {} image links, done!".format(len(image_urls)))
                     break
-                # else:
-                #     print("Found: {} image links, looking for more ...".format(len(image_urls)))
+                else:
+                    print("Found: {} image links, looking for more ...".format(len(image_urls)))
 
                 load_more_button = self.driver.find_elements(By.CSS_SELECTOR, ".mye4qd")
                 if load_more_button:
@@ -95,15 +84,14 @@ class ImageScraper:
         except Exception:
             return
 
-    def __add_image_urls_to_set(self, image_urls: set):
-        actual_images = self.driver.find_elements(By.CSS_SELECTOR, 'img.n3VNCb')
-        for actual_image in actual_images:
-            if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                image_urls.add(actual_image.get_attribute('src'))
+    def __add_image_urls_to_set(self, image_urls: set, count: int):
+        actual_images = self.driver.find_elements(By.CSS_SELECTOR, 'img._image')
+        print("actual_image size = ", len(actual_images))
+        for i in range(count):
+            image_urls.add(actual_images[i].get_attribute('src'))
 
     def __get_default_chrome_options(self):
         options = webdriver.ChromeOptions()
-        options.binary_location = '/opt/chrome/chrome'
         options.add_experimental_option("excludeSwitches", ['enable-automation'])
         options.add_argument('--disable-infobars')
         options.add_argument('--disable-extensions')
@@ -128,24 +116,12 @@ class ImageScraper:
 
         return options
 
-def lambda_handler(event, context):
-    body = json.loads(event["body"])
-    boto3_client = boto3.client("apigatewaymanagementapi", endpoint_url="https://onedv62i9e.execute-api.ap-northeast-2.amazonaws.com/production")
-    
-    s3_client = boto3.client('s3')
+def lambda_handler(keyword, count):
     scr = ImageScraper()
-    
-    keyword = body["message"]["keyword"]
-    count = int(body["message"]["count"])
-    
-    connectionId = event["requestContext"]["connectionId"]
-    
-    file_name = connectionId + keyword + '.zip'
-    
     urls = list(scr.get_image_urls(query=keyword, max_urls=count, sleep_between_interactions=1))
     images = []
     
-    for i in range(len(urls)):
+    for i in range(count):
         pil_image = scr.get_in_memory_image(urls[i], 'PNG')
         if not pil_image:
             continue
@@ -154,33 +130,20 @@ def lambda_handler(event, context):
         pil_image.close()
         images.append([str(i), file_object])
     
-    path = '/tmp/' + keyword + '.zip'    
+    path = keyword + '.zip'    
     with ZipFile(path, 'w') as zip_file:
         for i, (image_name, bytes_stream) in enumerate (images):
             if not images[i]:
                 continue
             zip_file.writestr(image_name+'.png', bytes_stream.getvalue())
-        # print(zip_file.infolist())
         
     print("Successfully loaded {} images".format(count))
     
-    
-    try:
-        upload_response = s3_client.upload_file(path, 'intizar-bucket', file_name)
-    except ClientError as e:
-        print(e)
-        
-    try:
-        url_response = s3_client.generate_presigned_url('get_object', Params={'Bucket':'intizar-bucket', 'Key': file_name})
-        # print(url_response)
-        
-        response = boto3_client.post_to_connection(ConnectionId=connectionId, Data=json.dumps(
-            {
-                "url": url_response
-            }
-        ))
-        
-    except ClientError as e:
-        print(e)
-    
     return { "statusCode": 200 }
+
+def main():
+    lambda_handler("cherry", 10)
+    return
+
+if __name__ == "__main__":
+    main()
